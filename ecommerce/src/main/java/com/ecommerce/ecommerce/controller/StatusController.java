f0af723e8c046be45ebbe00cc27841842e16d8c6
lusferr <fer.almeidabr@hotmail.com>
2022-01-31 19:03:15 -0300

    add controllers

--- ecommerce/src/main/java/com/ecommerce/ecommerce/controller/StatusController.java
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/controller/StatusController.java
@@ -2,8 +2,11 @@ package com.ecommerce.ecommerce.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +18,19 @@ import com.ecommerce.ecommerce.repository.StatusRepository;
 @CrossOrigin("*")
 public class StatusController {
 
+	@Autowired
 	private StatusRepository repository;
 	
+	@GetMapping
 	public ResponseEntity<List<Status>> getAll(){
 		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Status> GetById(@PathVariable long id){
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
 	}
 	
 }
--- ecommerce/src/main/java/com/ecommerce/ecommerce/controller/UsuarioController.java
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/controller/UsuarioController.java
@@ -1,5 +1,36 @@
 package com.ecommerce.ecommerce.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.ecommerce.ecommerce.model.Usuario;
+import com.ecommerce.ecommerce.repository.UsuarioRepository;
+
+@RestController
+@RequestMapping("/status")
+@CrossOrigin("*")
 public class UsuarioController {
 
+	@Autowired
+	private UsuarioRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> GetById(@PathVariable long id){
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
 }
